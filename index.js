596a3e40e17c29f318c81a4ea7b5c91bccbc75f0
PoliukhovychAnna <116736506+PoliukhovychAnna@users.noreply.github.com>
2023-03-31 20:43:21 +0300

    solved 1 task

--- index.js
+++ index.js
@@ -4,6 +4,12 @@
 Натискання на кнопку "SHOW ME" має виводити значення з поля введення (дивіться на елементи в html-розмітці)
 */
 
+const btnEl = document.querySelector("#alertButton");
+const inputEl = document.querySelector("#alertInput");
+
+btnEl.addEventListener("click", () => alert(inputEl.value)) 
+
+
 //TODO:==============================================
 /*
 Завдання 2
